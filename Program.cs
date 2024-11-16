99c70989093f3b77c91d99c313c9beadbd0b9de7
lucasdiniz1708 <lucasmoraes1708@gmail.com>
2024-11-16 14:03:26 -0300

    DesafioPOO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero:"123456",modelo:"Modelo1",imei:"11111111",memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Clash Royale");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero:"78910",modelo:"Modelo2",imei:"2222222",memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
